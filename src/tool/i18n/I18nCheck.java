816861d6f3a7a21296c5612ac350879d5598abce
lyrieek <tianhao7@foxmail.com>
2017-12-19 17:54:27 +0800

    separate i18n check

---
+++ src/tool/i18n/I18nCheck.java
@@ -0,0 +1,95 @@
+package tool.i18n;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+
+public class I18nCheck {
+
+	public static void main(String[] args) throws Exception {
+		for (String item : IOUtils.toString(new FileInputStream("G:/th/CmsApplicationTest/src/pers/th/i18n.txt"),"utf-8").split(System.lineSeparator())) {
+			String[] field = item.replace("$scope.", "").replace("translations.", "").replace(";", "").split("=");
+			if (field.length != 2) {
+				continue;
+			}
+			if (!field[0].trim().equals(field[1].trim())) {
+				System.out.println(item);
+			}
+		}
+	}
+
+	public static void eachFile(List<File> listFile, FileHandle handle) {
+		for (File file : listFile) {
+			if (handle.condition(file)) {
+				handle.analysis(file);
+			}
+		}
+	}
+
+	public static List<File> listFile(String path) {
+		List<File> files = new ArrayList<>();
+		File file = getFile(path);
+		if (file == null || !file.isDirectory()) {
+			return null;
+		}
+		for (File items : file.listFiles()) {
+			if (items.isDirectory()) {
+				files.addAll(listFile(items.getAbsolutePath()));
+			} else {
+				files.add(items);
+			}
+		}
+		return files;
+	}
+
+	public static File getFile(String path) {
+		if (path == null || path.trim().isEmpty()) {
+			return null;
+		}
+		File file = new File(path);
+		if (!file.exists()) {
+			return null;
+		}
+		return file;
+	}
+
+	public static String reader(String path) {
+		File file = getFile(path);
+		if (file == null || !file.isFile()) {
+			return "";
+		}
+		int length = 0;
+		byte[] buffer = new byte[512];
+		StringBuffer template = new StringBuffer();
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(path);
+			while ((length = fis.read(buffer)) != -1) {
+				template.append(new String(buffer, 0, length));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (fis != null) {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return template.toString();
+	}
+
+}
+
+interface FileHandle {
+
+	boolean condition(File item);
+
+	void analysis(File item);
+
+}
